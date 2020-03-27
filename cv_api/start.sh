b7cb5e749ba7c0a2f64a3156ac95adfd435eef00
orbit <orbit@localhost>
2020-03-27 15:55:21 -0700

    docker update

--- cv_api/start.sh
+++ cv_api/start.sh
@@ -1,3 +1,3 @@
 curl https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv -o state.csv
 curl https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv -o county.csv
-flask run --host=0.0.0.0
+export FLASK_RUN_PORT=$PORT && flask run --host=0.0.0.0
