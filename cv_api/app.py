214a98587d549de85bf6d9d4badeb5b3b0f92f3d
Brian DeRocher <brian@derocher.org>
2020-04-05 23:25:31 -0400

    fix pep-8 whitespace issues

--- cv_api/app.py
+++ cv_api/app.py
@@ -5,75 +5,93 @@ from flask import abort, Flask, request, render_template
 
 app = Flask(__name__)  # pylint: disable=invalid-name
 
+
 @app.route('/')
 def root():
     return render_template('index.html')
+
 
 @app.route('/api')
 def index():
     return render_template('index.html')
 
+
 @app.route('/api/total/counties')
 def total_counties():
     return process_counties_total(read_macro('county'), get_args())
+
 
 @app.route('/api/total/counties/<state>')
 def total_counties_state(state):
     return process_state_counties_total(read_macro('county'), state, None, get_args())
 
+
 @app.route('/api/total/counties/<state>/<county>')
-def  total_counties_state_county(state, county):
+def total_counties_state_county(state, county):
     return process_state_counties_total(read_macro('county'), state, county, get_args())
+
 
 @app.route('/api/total/states')
 def total_states():
     return country_view_total(read_macro('country'), get_args())
 
+
 @app.route('/api/total/states/<state>')
 def total_states_state(state):
     return state_view_total(read_macro('country'), state, get_args())
+
 
 @app.route('/api/total/states/<state>/counties')
 def total_states_state_counties(state):
     return process_state_counties_total(read_macro('county'), state, None, get_args())
 
+
 @app.route('/api/total/states/<state>/counties/<county>')
 def total_states_state_counties_county(state, county):
     return process_state_counties_total(read_macro('county'), state, county, get_args())
+
 
 @app.route('/api/timeline/counties')
 def timeline_counties():
     return process_country_county(read_macro('county'), get_args())
 
+
 @app.route('/api/timeline/counties/<state>')
 def timeline_counties_state(state):
     return process_state_county(read_macro('county'), state, None, get_args())
+
 
 @app.route('/api/timeline/counties/<state>/<county>')
 def timeline_counties_state_county(state, county):
     return process_state_county(read_macro('county'), state, county, get_args())
 
+
 @app.route('/api/timeline/states')
 def timeline_states():
     return country_view(read_macro('country'), get_args())
+
 
 @app.route('/api/timeline/states/<state>')
 def timeline_state(state):
     return state_view(read_macro('country'), state, get_args())
 
+
 @app.route('/api/timeline/states/<state>/counties')
 def timeline_state_counties(state):
     return process_state_county(read_macro('county'), state, None, get_args())
 
+
 @app.route('/api/timeline/states/<state>/counties/<county>')
 def timeline_state_county(state, county):
     return process_state_county(read_macro('county'), state, county, get_args())
+
 
 def state_view_total(data, state_filter, args):
     data = filter_country_state(data, state_filter)
     result = process_mode(args, data[-1][3], data[-1][4])
     result = str(result) if isinstance(result, int) else result
     return result
+
 
 def state_view(data, state_filter, args):
     result = {}
@@ -82,6 +100,7 @@ def state_view(data, state_filter, args):
         result[row[0]] = process_mode(args, row[3], row[4])
     return result
 
+
 def country_view_total(data, args):
     dataset = {}
     key_row = get_key_row(args, 'country')
@@ -89,6 +108,7 @@ def country_view_total(data, args):
         if row[key_row] not in dataset:
             dataset[row[key_row]] = process_mode(args, row[3], row[4])
     return dataset
+
 
 def country_view(data, args):
     dataset = {}
@@ -99,6 +119,7 @@ def country_view(data, args):
         dataset[row[key_row]][row[0]] = process_mode(args, row[3], row[4])
     return dataset
 
+
 def process_state_counties_total(data, state_filter, county_filter, args):
     data = filter_state(data, state_filter)
     if county_filter:
@@ -108,6 +129,7 @@ def process_state_counties_total(data, state_filter, county_filter, args):
         return result
     return process_state_data_total(data, args)
 
+
 def process_state_data_total(data, args):
     dataset = {}
     for row in reversed(data):
@@ -116,11 +138,13 @@ def process_state_data_total(data, args):
             dataset[row[key_row]] = process_mode(args, row[4], row[5])
     return dataset
 
+
 def process_state_county(data, state_filter, county_filter, args):
     data = filter_state(data, state_filter)
     if county_filter:
         return process_county_data(data, county_filter, args)
     return process_state_data(data, args)
+
 
 def process_county_data_total(data, county_filter, args):
     for row in reversed(data):
@@ -128,12 +152,14 @@ def process_county_data_total(data, county_filter, args):
             return process_mode(args, row[4], row[5])
     return None
 
+
 def process_county_data(data, county_filter, args):
     dataset = {}
     for row in data:
         if compare_county(county_filter, row[1], row[3]):
             dataset[row[0]] = process_mode(args, row[4], row[5])
     return dataset
+
 
 def process_state_data(data, args):
     dataset = {}
@@ -145,6 +171,7 @@ def process_state_data(data, args):
             dataset[row[key_row]][row[0]] = process_mode(args, row[4], row[5])
     return dataset
 
+
 def process_counties_total(data, args):
     dataset = {}
     key_row = get_key_row(args, 'state')
@@ -155,6 +182,7 @@ def process_counties_total(data, args):
         if row[key_row] not in dataset[state_key]:
             dataset[state_key][row[key_row]] = process_mode(args, row[4], row[5])
     return dataset
+
 
 def process_country_county(data, args):
     dataset = {}
@@ -168,12 +196,14 @@ def process_country_county(data, args):
         dataset[state_key][row[key_row]][row[0]] = process_mode(args, row[4], row[5])
     return dataset
 
+
 def process_mode(args, cases, deaths):
     if args['mode'] == 'cases':
         return int(cases)
     if args['mode'] == 'deaths':
         return int(deaths)
     return {'cases': cases, 'deaths': deaths}
+
 
 def filter_state(data, state_filter):
     result = []
@@ -182,12 +212,14 @@ def filter_state(data, state_filter):
             result.append(row)
     return result
 
+
 def filter_country_state(data, state_filter):
     result = []
     for row in data:
         if compare_state(state_filter, row[1]):
             result.append(row)
     return result
+
 
 def read_macro(macro):
     cv_data = []
@@ -197,6 +229,7 @@ def read_macro(macro):
             cv_data.append(row)
     cv_data.pop(0)
     return cv_data
+
 
 def get_macro_file(macro):
     file = None
@@ -210,9 +243,11 @@ def get_macro_file(macro):
         abort(500)
     return file
 
+
 def get_args():
     return {'mode': request.args.get('mode', None),
             'fips': request.args.get('fipsKey', False)}
+
 
 def compare_state(state_filter, entry):
     if str_normalize(entry) == str_normalize(state_filter):
@@ -221,6 +256,7 @@ def compare_state(state_filter, entry):
         return True
     return False
 
+
 def compare_county(county_filter, entry, fips_entry):
     if str_normalize(entry) == str_normalize(county_filter):
         return True
@@ -228,8 +264,10 @@ def compare_county(county_filter, entry, fips_entry):
         return True
     return False
 
+
 def str_normalize(words):
     return words.replace(' ', '').lower().capitalize()
+
 
 def get_key_row(args, locale):
     if locale == 'state':
@@ -237,6 +275,7 @@ def get_key_row(args, locale):
     else:
         key_row = 2 if args['fips'] else 1
     return key_row
+
 
 def get_state_key(args, state):
     if args['fips']:
